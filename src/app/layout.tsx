7ebf630cd7ec0d3b16e5643e583e17b3004335ec
BMO <meow@nermalcat69.dev>
2025-11-20 15:00:04 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -81,11 +81,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-<script>
-(function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="lPrIAdAutSXrhe4GaeBne";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();
-</script>
-      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
@@ -97,6 +92,15 @@ export default function RootLayout({
         )}
       >
         <RootProviders>{children}</RootProviders>
+        	<Script>
+	{
+		`(function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");
+    script.src="${(process.env.NEXT_PUBLIC_CHATBASE_HOST || 'https://www.chatbase.co/') + 'embed.min.js'}";
+    script.id="${ process.env.NEXT_PUBLIC_CHATBOT_ID}";
+    script.domain="${process.env.NEXT_PUBLIC_CHATBASE_HOST || 'www.chatbase.co'}";
+    document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();`
+	}
+	  </Script>
         <Analytics />
       </body>
     </html>
