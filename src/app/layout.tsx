a9b8a1978094e26be7bd038fdd397a0cbd55fdb2
BMO <meow@nermalcat69.dev>
2025-11-20 14:38:20 +0530

    setup chatbase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ import { cn } from "@/lib/utils";
 import RootProviders from "@/components/providers";
 import { ChatBot } from "@/components/chatbot";
 import { ChatBotRandom } from "@/components/randomize";
+import Script from "next/script";
 
 const fontSans = Inter({
   subsets: ["latin"],
@@ -91,11 +92,13 @@ export default function RootLayout({
         )}
       >
         <RootProviders>{children}</RootProviders>
-<iframe
-    src="https://www.chatbase.co/chatbot-iframe/lPrIAdAutSXrhe4GaeBne"
-    width="100%"
-    className="h-full min-h-[700px] border-none"
-></iframe>
+<Script>
+{
+`(function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="www.chatbase.co/embed.min.js";script.id="${
+process.env.NEXT_PUBLIC_CHATBOT_ID
+}";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();`
+}
+</Script>
         <Analytics />
       </body>
     </html>
