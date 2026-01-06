a88fe08ebd20a54ad6c0b40fe8b74afe0fdba110
BMO <meow@nermalcat69.dev>
2026-01-06 15:28:16 +0530

    remove chatbase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -106,13 +106,13 @@ export default function RootLayout({
             })();
           `}
         </Script>
-        <Script>
+        {/* <Script>
           {`(function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");
     script.src="${(process.env.NEXT_PUBLIC_CHATBASE_HOST || "https://www.chatbase.co/") + "embed.min.js"}";
     script.id="${process.env.NEXT_PUBLIC_CHATBOT_ID}";
     script.domain="${process.env.NEXT_PUBLIC_CHATBASE_HOST || "www.chatbase.co"}";
     document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();`}
-        </Script>
+        </Script> */}
         <Analytics />
       </body>
     </html>
