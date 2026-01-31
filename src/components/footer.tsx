123aba3674c1736a294f717b52ecd208609fc3c1
BMO <meow@nermalcat69.dev>
2026-01-31 17:23:57 +0530

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -15,7 +15,8 @@ export default function Footer() {
   ];
 
   const resourceLinks = [
-    { href: "https://b2b.graycup.org/", label: "B2B Store" },
+    { href: "https://b2b.graycup.in/", label: "B2B Store" },
+    { href: "https://graycup.in/", label: "Consumer Store" },
     { href: "https://status.graycup.org/", label: "Status" },
     { href: "/sitemap.xml", label: "Sitemap" },
   ];
